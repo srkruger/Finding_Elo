0832595b237a839f181a6bde882171915cf8082e
Frans Slothouber <frans.slothouber@gmail.com>
2014-10-26 02:42:59 +0100

    Visualize ELO Distribution

---
+++ Explore/README.md
@@ -0,0 +1,3 @@
+
+This directory contains programs to explore the dataset.
+That is making visualizations and computing statistics.
--- Explore/visualize.r
+++ Explore/visualize.r
@@ -1,8 +1,12 @@
 
 require(ggplot2)
 
-dataset <- read.table("../Processed/move_count.csv", header=TRUE, sep=",")
+dataset <-read.table("../Processed/move_count.csv", header=TRUE, sep=",") 
 
 p1 <- ggplot(dataset, aes(x=move_count)) + geom_histogram(binwidth=1)
 ggsave(plot=p1, file="move_count_distribution.pdf")
 
+dataset <- read.table("../Processed/elo.csv", header=TRUE, sep=",")
+
+p2 <- ggplot(dataset,aes(x=WhiteElo))+geom_histogram(binwidth=10)
+ggsave(plot=p2, file="white_elo_distribution.pdf")
